fbd43c8c84bf7ca0247a39fb35a18668faea8274
Romain Dénarié <romain.denarie@exoplatform.com>
2023-06-05 17:04:15 +0200

    Add eslint-vue-a11y plugin and rules to default cofniguration

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ module.exports = {
   extends: [
     'plugin:vue/strongly-recommended',
     'eslint:recommended',
+    "plugin:vuejs-accessibility/recommended",
   ],
   rules: {
     'indent': [
@@ -101,7 +102,28 @@ module.exports = {
       'singleline': 'never',
       'multiline': 'never'
     }],
-    'vue/component-name-in-template-casing': ['error', 'kebab-case']
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    "vuejs-accessibility/alt-text": "warn",
+    "vuejs-accessibility/anchor-has-content": "warn",
+    "vuejs-accessibility/aria-props": "warn",
+    "vuejs-accessibility/aria-role": "warn",
+    "vuejs-accessibility/aria-unsupported-elements": "warn",
+    "vuejs-accessibility/click-events-have-key-events": "warn",
+    "vuejs-accessibility/form-control-has-label": "warn",
+    "vuejs-accessibility/heading-has-content": "warn",
+    "vuejs-accessibility/iframe-has-title": "warn",
+    "vuejs-accessibility/interactive-supports-focus": "warn",
+    "vuejs-accessibility/label-has-for": "warn",
+    "vuejs-accessibility/media-has-caption": "warn",
+    "vuejs-accessibility/mouse-events-have-key-events": "warn",
+    "vuejs-accessibility/no-access-key": "warn",
+    "vuejs-accessibility/no-autofocus": "warn",
+    "vuejs-accessibility/no-distracting-elements": "warn",
+    "vuejs-accessibility/no-onchange": "warn",
+    "vuejs-accessibility/no-redundant-roles": "warn",
+    "vuejs-accessibility/no-static-element-interactions": "warn",
+    "vuejs-accessibility/role-has-required-aria-props": "warn",
+    "vuejs-accessibility/tabindex-no-positive": "warn"
   },
   'parserOptions': {
     'sourceType': 'module',
